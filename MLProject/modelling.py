e19ca94b35ae8d336084d499c3d44cce3ad866b9
MiftakhulAR <miftakhul.arigin@gmail.com>
2025-12-19 08:42:25 +0700

    Refactor MLflow logging and model training

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -17,52 +17,36 @@ X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.2, random_state=42
 )
 
-# Manual logging (Advance)
-with mlflow.start_run():
-    model = RandomForestClassifier(
-        n_estimators=100,
-        max_depth=5,
-        random_state=42
-    )
-    model.fit(X_train, y_train)
-
-    y_pred = model.predict(X_test)
-
-    acc = accuracy_score(y_test, y_pred)
-    prec = precision_score(y_test, y_pred)
-    rec = recall_score(y_test, y_pred)
-    f1 = f1_score(y_test, y_pred)
-
-    mlflow.log_param("n_estimators", 100)
-    mlflow.log_param("max_depth", 5)
-
-    mlflow.log_metric("accuracy", acc)
-    mlflow.log_metric("precision", prec)
-    mlflow.log_metric("recall", rec)
-    mlflow.log_metric("f1_score", f1)
-
-    # Confusion Matrix (Artefak tambahan)
-    cm = confusion_matrix(y_test, y_pred)
-    plt.figure(figsize=(5,4))
-    sns.heatmap(cm, annot=True, fmt="d")
-    plt.title("Confusion Matrix")
-    plt.savefig("confusion_matrix.png")
-    mlflow.log_artifact("confusion_matrix.png")
-
-    # Save model
-    mlflow.sklearn.log_model(model, "model")
-
-import os
-
-with mlflow.start_run() as run:
-    run_id = run.info.run_id
-
-    # simpan run_id ke file
-    with open("run_id.txt", "w") as f:
-        f.write(run_id)
-
-mlflow.sklearn.log_model(
-    sk_model=model,
-    artifact_path="model",
-    registered_model_name=None
+# Train model (MLflow Project SUDAH buat run otomatis)
+model = RandomForestClassifier(
+    n_estimators=100,
+    max_depth=5,
+    random_state=42
 )
+model.fit(X_train, y_train)
+
+y_pred = model.predict(X_test)
+
+acc = accuracy_score(y_test, y_pred)
+prec = precision_score(y_test, y_pred)
+rec = recall_score(y_test, y_pred)
+f1 = f1_score(y_test, y_pred)
+
+mlflow.log_param("n_estimators", 100)
+mlflow.log_param("max_depth", 5)
+
+mlflow.log_metric("accuracy", acc)
+mlflow.log_metric("precision", prec)
+mlflow.log_metric("recall", rec)
+mlflow.log_metric("f1_score", f1)
+
+# Confusion Matrix
+cm = confusion_matrix(y_test, y_pred)
+plt.figure(figsize=(5,4))
+sns.heatmap(cm, annot=True, fmt="d")
+plt.title("Confusion Matrix")
+plt.savefig("confusion_matrix.png")
+mlflow.log_artifact("confusion_matrix.png")
+
+# LOG MODEL (INI YANG DICARI DOCKER)
+mlflow.sklearn.log_model(model, artifact_path="model")
